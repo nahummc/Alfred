41420b9f3f9800fcddd2866d96c586c3166c42eb
nahummc <nahumcollicott@gmail.com>
2023-09-22 23:27:14 -0300

    Refactord alfred.py to use the openai library, not just a simple api call. Th reason is so I can use more text-model options

--- alfred.py
+++ alfred.py
@@ -1,70 +1,26 @@
 import os
 
-import dotenv
 import openai
 
-# import log
-# logger = log.setup_logger()
-
-dotenv.load_dotenv()
-# access open api
-openai.organization = os.getenv("OPENAI_ORG_KEY")
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
+# AlfredChat.py
 class AlfredChat:
-    # openai.error.InvalidRequestError: This is a chat  model and not supported in the
-    # v1 / completions endpoint. Did you mean to use v1 / chat / completions?
+    def __init__(self, api_key):
+        self.api_key = api_key
 
-    def __init__(self, engine: str = "text-davinci-003", tokens: int = 300):
-        self.engine = engine
-        # self.prompt = prompt
-        self.tokens = tokens
+    # @staticmethod
+    def return_completion(self, prompt: str):
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "user", "content": prompt
+                }
+            ]
+        )
 
-    def __str__(self):
-        completion = openai.Completion.create(
-            engine=self.engine, prompt=self.prompt, max_tokens=1000)
-        return f"{completion.choices[0].text}"
-
-    def return_completion(self, prompt: str, custom_prompt: str = None, engine: str = None, tokens: int = None):
-        if custom_prompt is not None:
-            prompt = custom_prompt + prompt + "Please do not include the entire previous context in your next message unless asked about it or explicitly told to. "
-
-        # Use the class variables instead of the non-existent _engine and _tokens
-        engine = engine if engine is not None else self.engine
-        tokens = tokens if tokens is not None else self.tokens
-
-        completion = openai.Completion.create(
-            engine=engine, prompt=prompt, max_tokens=tokens)
-        return f"{completion.choices[0].text}"
-
-    # def get_sentiment(self):
-    #     data = {
-    #         "model": self.engine,
-    #         # ...
-    #     }
-    #     headers = {
-    #         "Authorization": f"Bearer {openai.api_key}"
-    #     }
-    #     response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=data)
-    #     result = response.json()
-    #
-    #     if 'error' in result:
-    #         logger.error(f"API Error: {result['error']}")
-    #         return 0
-    #
-    #     try:
-    #         score = int(result.get('choices', [{}])[0].get('message', {}).get('content', '0').strip())
-    #         logger.info(f"Sentiment Score: {score}")
-    #     except (ValueError, TypeError, KeyError) as e:
-    #         logger.error(f"Exception occurred: {e}")
-    #         score = 0
-    #
-    #     return score
-    #
-    # def analyze_sentiment(prompt: str):
-    #     score = AlfredChat(prompt=prompt).get_sentiment()
-    #     logger.info(f"Received score: {score}, type: {type(score)}")
-    #     return score
+        return response['choices'][0]['message']['content']
 
 
